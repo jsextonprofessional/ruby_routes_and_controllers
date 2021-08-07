9a6fb43db672d879a29ae0778934cbce23bcea9d
Jacob Sexton <jshuman@protonmail.com>
2021-08-07 01:19:50 +0000

    Fixed session stuff, and changed name route to be dynamic rather than hard coded

--- app/controllers/hello_controller.rb
+++ app/controllers/hello_controller.rb
@@ -3,20 +3,30 @@ class HelloController < ApplicationController
     def index
         puts 'were at index'
     end
+
     def hello
         render 'hello'
     end
+    
     def say
-        render 'say'
+        unless params[:name]
+            render text: "Saying Hello!"
+        else
+            if params[:name] == "michael"
+                redirect_to "/say/hello/joe"
+            else
+                render text: "SAYING HELLO #{params[:name]}!"
+            end
+        end
     end
-    def joe
-        render 'joe'
-    end
-    def michael
-        redirect_to '/say/hello/joe'
-    end
+    
     def times
-        session[:counter]
-        render 'times'
+        session[:count] ||= 0
+        render text: "YOU HAVE VISITED THIS URL #{session[:count] += 1} time(s)"
+    end
+    
+    def restart
+    reset_session
+    render text: "SESSION DESTROYED"
     end
 end
